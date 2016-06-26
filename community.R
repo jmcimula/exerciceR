ae0916f7e32aed15c8740836d49f14c18f50debf
Jean Marie Cimula <jmkacim@gmail.com>
2016-06-26 14:12:34 +0200

    Some R communities and forums

---
+++ community.R
@@ -0,0 +1,15 @@
+#R Community & Forum
+
+[Revolution Analytics ] (http://www.revolutionanalytics.com/r-community)
+
+[R Inside] (http://www.inside-r.org/)
+
+[R Dir] (https://r-dir.com/community/forums.html)
+
+[R Help] (https://stat.ethz.ch/mailman/listinfo/r-help)
+
+[Nabble] (http://r.789695.n4.nabble.com/)
+
+[Stackoverflow] (http://stackoverflow.com/questions/tagged/r)
+
+[R Studio] (https://www.rstudio.com)
